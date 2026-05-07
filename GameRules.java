8d25689cd844f80ab3aa4039180e3d215db3e4c3
Layan-387 <lalserhani2005@hotmail.com>
2026-05-07 19:15:20 +0300

    Update GameRules.java

--- GameRules.java
+++ GameRules.java
@@ -1,8 +1,8 @@
 public class GameRules {
-
+// check if there is a forbidden letter
     public static boolean containsForbiddenLetter(String word, char[] forbiddenLetters) {
         word = word.toLowerCase();
-
+// walk on array forbidden letter and check if word match
         for (char letter : forbiddenLetters) {
             if (word.indexOf(Character.toLowerCase(letter)) != -1) {
                 return true;
@@ -10,11 +10,11 @@ public class GameRules {
         }
         return false;
     }
-
+// compare len of word with minlength
     public static boolean meetsMinLength(String word, int minLength) {
         return word.length() >= minLength;
     }
-
+// check if word is realated to topic
     public static boolean isTopicRelated(String word, String[] acceptedWords) {
         word = word.toLowerCase();
 
