729ff0b2cc40966bea3335fcbd5d131d63925346
Layan-387 <lalserhani2005@hotmail.com>
2026-05-07 19:05:01 +0300

    Update LevelConfig.java

--- LevelConfig.java
+++ LevelConfig.java
@@ -1,11 +1,12 @@
 public class LevelConfig {
-    private int levelNumber;
-    private String topic;
-    private char[] forbiddenLetters;
-    private int minLength;
-    private int levelTimeSeconds;
-    private String[] acceptedWords;
+    private int levelNumber; // nummber of levels 1 2 3 4 5
+    private String topic; // eexamplex fruits 
+    private char[] forbiddenLetters; // example A so client cant write A
+    private int minLength; // if its 5 -> player can write 5 or less
+    private int levelTimeSeconds; // sec for this level ( 2 & 5 )
+    private String[] acceptedWords; 
 
+    // constructor
     public LevelConfig(int levelNumber, String topic, char[] forbiddenLetters,
                        int minLength, int levelTimeSeconds, String[] acceptedWords) {
         this.levelNumber = levelNumber;
@@ -15,14 +16,14 @@ public class LevelConfig {
         this.levelTimeSeconds = levelTimeSeconds;
         this.acceptedWords = acceptedWords;
     }
-
+// getters so we can read level info outside the class
     public int getLevelNumber() { return levelNumber; }
     public String getTopic() { return topic; }
     public char[] getForbiddenLetters() { return forbiddenLetters; }
     public int getMinLength() { return minLength; }
     public int getLevelTimeSeconds() { return levelTimeSeconds; }
     public String[] getAcceptedWords() { return acceptedWords; }
-
+// collect all details of level in one string 
     public String toProtocolMessage() {
         return "LEVEL_RULES:" + levelNumber + ":" + topic + ":" +
                 new String(forbiddenLetters) + ":" + minLength + ":" + levelTimeSeconds;
