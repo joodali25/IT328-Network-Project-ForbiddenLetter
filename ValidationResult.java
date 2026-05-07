0f691a3343023167165da13d33832a52da129555
Layan-387 <lalserhani2005@hotmail.com>
2026-05-07 18:40:45 +0300

    Create ValidationResult.java

---
+++ ValidationResult.java
@@ -0,0 +1,17 @@
+public class ValidationResult {
+    private boolean valid;
+    private String message;
+
+    public ValidationResult(boolean valid, String message) {
+        this.valid = valid;
+        this.message = message;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
