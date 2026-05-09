733b549fd3e0f57802b006230c47eab61ababb98
joodali25 <jood0505ali@gmail.com>
2026-05-09 22:27:17 +0300

    Remove package declaration from ValidationResult.java
    Remove package declaration from ValidationResult.java

--- ValidationResult.java
+++ ValidationResult.java
@@ -1,5 +1,3 @@
-package com.mycompany.networkpro2;
-
 public class ValidationResult {
     private boolean valid; // if the world valid 
     private String message; // if its wromg or right send a message
